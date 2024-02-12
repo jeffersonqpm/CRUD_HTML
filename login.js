586f5998f16412e4a77c634aee8a2ffbee07dab6
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-02-12 01:27:51 -0300

    sdrghjdfghj

--- login.js
+++ login.js
@@ -3,29 +3,37 @@
 // senha = document.getElementById('senha').value;
 // let senha = document.getElementById('senha');
 
-const login = document.formLogin.txtLogin.value;
-const senha = document.formLogin.txtSenha.value;
 
-// const login = document.getElementById('login');
-// const senha = document.getElementById('senha');
+
+// const login = document.getElementById('exampleInputEmail1');
+// const senha = document.getElementById('exampleInputPassword1');
 
 // const loginn = document.getElementById('login').value;
 // const senhaa = document.getElementById('senha').value;
 
-const butaoLoga = document.getElementById('bt-login');
+const butaoLoga = document.getElementById('button-entrar');
 
-function logar(){
+function logar() {
 
-    if(login === "123" && senha === "123"){
+    // const login = document.formLogin.txtLogin.value;
+    // const senha = document.formLogin.txtSenha.value;
 
+    const login = document.getElementById('exampleInputEmail1').value;
+    const senha = document.getElementById('exampleInputPassword1').value;
+
+    if (senha === "123" && login == "123") {
+
+        alert('logado');
         window.location.href = "page1.html";
 
-    }else{
+    } else {
         alert('Login inválido!')
     }
+
+
 }
 
-butaoLoga.addEventListener('click', logar);
+//butaoLoga.addEventListener('click', logar); return false
 
 
 
